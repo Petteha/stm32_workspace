ba5d3bafb97f0148d520671fba18fad005d77995
Peter Ha <Peterhazu@gmail.com>
2026-09-15 13:41:15 +0200

    .

--- testdisplay_lcd1602/Core/Src/main.c
+++ testdisplay_lcd1602/Core/Src/main.c
@@ -18,9 +18,16 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <string.h>
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+
+void LCD_Init();
+void LCD_WriteString(const char *text);
+void LCD_SendByte(uint8_t value);
+void LCD_PulseEnable(void);
+void LCD_Send4Bits(uint8_t value);
 
 /* USER CODE END Includes */
 
@@ -64,8 +71,87 @@ static void MX_GPIO_Init(void);
   * @retval int
   */
 
-void LCD_Send4Bits(uint8_t value)
+void LCD_Init()
 {
+
+	// Initialization
+	HAL_Delay(50);
+
+	HAL_GPIO_WritePin(lcd_rs_GPIO_Port, lcd_rs_Pin, GPIO_PIN_RESET);
+
+	LCD_Send4Bits(0x03);
+	LCD_PulseEnable();
+	HAL_Delay(5);
+
+	LCD_Send4Bits(0x03);
+	LCD_PulseEnable();
+	HAL_Delay(1);
+
+	LCD_Send4Bits(0x03);
+	LCD_PulseEnable();
+	HAL_Delay(1);
+
+	LCD_Send4Bits(0x02);
+	LCD_PulseEnable();
+	HAL_Delay(1);
+
+	// Setup/configuration
+	// 4 bit - 2 line display
+	LCD_SendByte(0x28);
+	// Display off
+	LCD_SendByte(0x08);
+	// Clear display
+	LCD_SendByte(0x01);
+	// Cursor moves right
+	LCD_SendByte(0x06);
+	// Display on
+	LCD_SendByte(0x0c);
+}
+
+void LCD_WriteString(const char *text) {
+	// Iterate over each letter/number
+	size_t length = strlen(text);
+
+	HAL_GPIO_WritePin(lcd_rs_GPIO_Port, lcd_rs_Pin, GPIO_PIN_SET);
+
+	for (int i = 0; i < length; i++)
+	{
+		// send char to LCD_sendbyte
+		LCD_SendByte((uint8_t)text[i]);
+	}
+	//
+}
+
+void LCD_SendByte(uint8_t value)
+{
+	uint8_t upper = value >> 4;
+	uint8_t lower = value & 0x0F;
+
+    // Take upper 4 bits and send to LCD_Send4Bits
+	LCD_Send4Bits(upper);
+    // pulse E
+	LCD_PulseEnable();
+
+    // Take lower 4 bits and send to LCD_Send4Bits
+	LCD_Send4Bits(lower);
+    // pulse E
+	LCD_PulseEnable();
+}
+
+void LCD_PulseEnable(void)
+{
+    HAL_GPIO_WritePin(lcd_e_GPIO_Port, lcd_e_Pin, GPIO_PIN_SET);
+
+    // Small delay
+    HAL_Delay(1);
+
+    HAL_GPIO_WritePin(lcd_e_GPIO_Port, lcd_e_Pin, GPIO_PIN_RESET);
+
+    // Small delay
+    HAL_Delay(1);
+}
+
+void LCD_Send4Bits(uint8_t value) {
 	if (value & 0x01)
 		{
 			HAL_GPIO_WritePin(lcd_d4_GPIO_Port, lcd_d4_Pin, GPIO_PIN_SET);
@@ -87,7 +173,6 @@ void LCD_Send4Bits(uint8_t value)
 		}	else	{
 			HAL_GPIO_WritePin(lcd_d6_GPIO_Port, lcd_d6_Pin, GPIO_PIN_RESET);
 		}
-
 
 	if (value & 0x08)
 		{
@@ -112,6 +197,7 @@ int main(void)
 
   /* USER CODE BEGIN Init */
 
+
   /* USER CODE END Init */
 
   /* Configure the system clock */
@@ -125,8 +211,9 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+  LCD_Init();
 
-  uint32_t test = 0x0001;
+  LCD_WriteString("HELLO BABA");
 
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_0, GPIO_PIN_SET);
 
